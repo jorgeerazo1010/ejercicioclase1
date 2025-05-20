85363679bba09ac370e3f7b00d7d8aba772122d1
jorgeerazo1010 <00047225@uca.edu.sv>
2025-05-20 12:29:41 -0600

    ejercicio for 🦁

---
+++ for2.cpp
@@ -0,0 +1,31 @@
+#include <iostream> //Bibliotecas
+using namespace std;
+int n, primo, modulo;
+int main()
+{
+    primo = 1;
+    cout << "Ingrese el numero entero:" << endl;
+    cin >> n;
+    for (int i = n - 1; i >= 2; i--)
+    {
+        modulo = n % i;
+        if (modulo == 0)
+        {
+            cout << "No es primo" << endl;
+            primo = 0;
+            i = 2;
+        }
+        else
+        {
+
+        }}
+            if (primo == 1)
+            {
+                cout << "Es primo" << endl;
+                primo = 0;
+            }
+            else {
+
+            }
+    return 0;
+}
